ca9d7d14921a09d01a2e591b2effe0f10b2e8c8b
Ben Parisot <me@benparisot.com>
2019-07-15 16:18:40 -0700

    install dependencies, begin Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import github from '../assets/github.png';
+import twitter from '../assets/twitter.png';
+import linkedin from '../assets/linkedin.png';
+import headerLogo from '../assets/Ben-Parisot-Logo2-WEB.png';
+
