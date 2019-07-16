3a7d1a93a68f785d2ef17636625aebc94af6a154
Ben Parisot <me@benparisot.com>
2019-07-16 08:17:24 -0700

    render header on app.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import github from '../assets/github.png';
 import twitter from '../assets/twitter.png';
 import linkedin from '../assets/linkedin.png';
@@ -8,9 +7,9 @@ import headerLogo from '../assets/Ben-Parisot-Logo2-WEB.png';
 export default function Header() {
     return (
        <header>
-        <Link to="/">
+        <a href="/">
           <img id="home" style={{ height: '75px', margin: 'auto' }} src={headerLogo} alt="Ben Parisot Logo" />
-        </Link>
+        </a>
         <nav>
             <a href="https://twitter.com/HereCodesNothin" target="blank"><img style={{ width: '45px', height: '45px', margin: 'auto 7px' }} src={twitter} alt="Twitter logo" /></a>
             <a href="https://www.linkedin.com/in/benparisot/" target="blank"><img style={{ width: '45px', height: '45px', margin: 'auto 7px' }} src={linkedin} alt="LinkedIn logo" /></a>
