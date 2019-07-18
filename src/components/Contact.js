81657405f61997f64967f8c099e5e95c43d65cc1
Ben Parisot <me@benparisot.com>
2019-07-18 11:54:40 -0700

    one more debug attempt

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,8 +12,8 @@ export default function Contact() {
                     <p><span className="contact-method">LinkedIn:</span> <a href="https://www.linkedin.com/in/benparisot/" target="blank">/benparisot</a></p>
                     <p><span className="contact-method">GitHub:</span> <a href="https://github.com/benparisot" target="blank">/benparisot</a></p>
                 </section>
-                <form name="contact" method="post" encType="application/x-www-form-urlencoded" netlify="true" data-netlify="true">
-                    <input type="hidden" name="contact" value="contact" />
+                <form name="contact" method="POST" encType="application/x-www-form-urlencoded" netlify="true" data-netlify="true">
+                    <input type="hidden" form-name="contact" value="contact" />
                     <input type="text" name="name" defaultValue="Your Name" />
                     <input type="text" name="email" defaultValue="Your Email" />
                     <textarea name="message" defaultValue="Your Message" />
