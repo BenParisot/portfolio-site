387dc72840221716e32be1cb7a5e79dc6bcc35ce
Ben Parisot <me@benparisot.com>
2019-07-18 11:02:07 -0700

    create home page to route contact form submission to success page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Hr from './components/Hr';
-import Hero from './components/Hero';
-import About from './components/About';
-import Projects from './components/Projects';
-import Work from './components/Work';
-import Contact from './components/Contact';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import Home from './pages/Home';
+import Success from './pages/FormSuccess';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <Header />
-      <Hero />
-      <Hr color="#f04d4d" />
-      <About />
-      <Hr color="#f26639" />
-      <Projects />
-      <Hr color="#f26639" />
-      <Work />
-      <Hr color="#f04d4d" />
-      <Contact />
-      <Footer />
-    </div>
-  );
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/success" component={Success} />
+      </Switch>
+    </Router>
+  )
 }
-
-export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../App.css';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Hr from '../components/Hr';
+import Hero from '../components/Hero';
+import About from '../components/About';
+import Projects from '../components/Projects';
+import Work from '../components/Work';
+import Contact from '../components/Contact';
+
+function Home() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      <Hr color="#f04d4d" />
+      <About />
+      <Hr color="#f26639" />
+      <Projects />
+      <Hr color="#f26639" />
+      <Work />
+      <Hr color="#f04d4d" />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
