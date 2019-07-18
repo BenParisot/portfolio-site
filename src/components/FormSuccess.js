164f1b0f59ac4efaa7b42dd017ff14cda0ffc09c
Ben Parisot <me@benparisot.com>
2019-07-18 10:49:17 -0700

    creating home page

---
+++ src/components/FormSuccess.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Success() {
+    return (
+        <section>
+            <p>Thanks for your message! I will get back to you shortly</p>
+            <Link>Return Home</Link>
+        </section>
+    )
+}
