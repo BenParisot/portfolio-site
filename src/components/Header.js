ac74d8cb151b9b89466af85d32b193d2bee3b2ec
Ben Parisot <me@benparisot.com>
2019-07-24 13:57:34 -0700

    begin mobile hamburger nav menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,21 +3,25 @@ import github from '../assets/github.png';
 import twitter from '../assets/twitter.png';
 import linkedin from '../assets/linkedin.png';
 import headerLogo from '../assets/Ben-Parisot-Logo2-WEB.png';
+import icon from '../assets/icon.jpg';
 
 export default function Header() {
     return (
-       <header>
+       <header className="header">
         <a href="/">
-          <img id="home" style={{ height: '75px', margin: 'auto' }} src={headerLogo} alt="Ben Parisot Logo" />
+          <img className="logo-desktop" id="home" style={{ height: '75px', margin: 'auto' }} src={headerLogo} alt="Ben Parisot Logo" />
+          <img className="logo-mobile" src={icon} alt="Ben Parisot Logo small" />
         </a>
-        <nav>
+        <nav className="menu">
+          <input className="menu-btn" type="checkbox" id="menu-btn" />
+          <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
           <li><a href="/">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#projects">Projects</a></li>
           <li><a target="blank" href="https://herecodesnothing.com/">Blog</a></li>
           <li><a href="#contact">Contact</a></li>
         </nav>
-        <section>
+        <section className="social-links">
             <a href="https://twitter.com/HereCodesNothin" target="blank"><img style={{ width: '45px', height: '45px', margin: 'auto 7px' }} src={twitter} alt="Twitter logo" /></a>
             <a href="https://www.linkedin.com/in/benparisot/" target="blank"><img style={{ width: '45px', height: '45px', margin: 'auto 7px' }} src={linkedin} alt="LinkedIn logo" /></a>
             <a href="https://github.com/benparisot" target="blank"><img style={{ width: '45px', height: '45px', margin: 'auto 7px' }} src={github} alt="Github logo" /></a>
