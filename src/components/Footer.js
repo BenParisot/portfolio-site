31f16e53cf023a255a30664352893bb9c21e2e2a
Ben Parisot <me@benparisot.com>
2019-07-25 13:01:35 -0700

    finish styling footer, import google fonts

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,7 @@ import React from 'react';
 export default function Footer() {
     return (
         <footer>
-            <ul>
-                <li>&copy; Ben Parisot 2019</li>
-                <li>Built with Gatsby</li>
-                <li><a href="#home">Back to the Top</a></li>
-            </ul>
+            <p><a href="#header">Back to the Top</a></p>
         </footer>
     )
 }
