5b2b8fd3a920af9639eb7643f1ed0f5fd6b943b4
Ben Parisot <me@benparisot.com>
2019-07-30 14:35:15 -0700

    style projects section desktop

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,13 +7,17 @@ import meganap from '../assets/meganap.jpg';
 
 export default function Projects() {
     return (
-            <section id="projects" className="projects">
+            <section className="projects-container">
                 <h2>Recent Projects:</h2>
-                <Project title="Mega Nap" image={meganap} description="A web app that lets front end developers generate API endpoints from any data set." demo="Live Demo" repo="Github Repo" color="#ed4b64" />
-                <Project title="Robot Haikut" image={robot} description="A Twitter bot that sends a randomly-generated haiku to those that ask for one." demo="Twitter Account" repo="Github Repo" color="#f26639" />
-                <Project title="Track 2020" image={track2020} description="An app to help people track their favorite candidates during the 2020 democratic primary." demo="Live Demo" repo="Github Repo"color="#f04d4d" />
-                <Project title="Pokematch" image={pokemon} description="A Pokemon matching game to teach kids numbers and pattern recognition." demo="Live Demo" repo="Github Repo" color="#ed4b64" />
-                <section className="github-cta">View more of my code on GitHub.</section>
+                <section id="projects" className="projects">
+                    <Project title="Mega Nap" image={meganap} description="A web app that lets front end developers generate API endpoints from any data set." demo="Live Demo" repo="Github Repo" color="#ed4b64" />
+                    <Project title="Robot Haikut" image={robot} description="A Twitter bot that sends a randomly-generated haiku to those that ask for one." demo="Twitter Account" repo="Github Repo" color="#f26639" />
+                    <Project title="Track 2020" image={track2020} description="An app to help people track their favorite candidates during the 2020 democratic primary." demo="Live Demo" repo="Github Repo"color="#f04d4d" />
+                    <Project title="Pokematch" image={pokemon} description="A Pokemon matching game to teach kids numbers and pattern recognition." demo="Live Demo" repo="Github Repo" color="#ed4b64" />
+                </section>
+                <section className="github-cta">
+                    <a target="blank" href="https://github.com/benparisot"><button className="github-cta-button">View more of my code on GitHub</button></a>
+                </section>
             </section>
     )
 }
--- src/components/Work.js
+++ src/components/Work.js
@@ -7,15 +7,15 @@ export default class Work extends PureComponent {
         company: 'Helios Interactive',
         description: 'Part scrum master and part release manager, I worked with clients, producers, designers, and developers to keep projects on schedule and deliver working apps under strict deadlines.',
         dates: 'July 2016 - Jan 2019',
-        selected: null
+        // selected: null
     }
 
     handleClick = (e) => {
-        if(this.state.selected === e.currentTarget.dataset.position) {
-            this.setState({ selected: null })
-        } else {
-            this.setState({ selected: e.currentTarget.dataset.position })
-        }
+        // if(this.state.selected === e.currentTarget.dataset.position) {
+        //     this.setState({ selected: null })
+        // } else {
+        //     this.setState({ selected: e.currentTarget.dataset.position })
+        // }
         this.setState({
             title: e.currentTarget.dataset.title,
             company: e.currentTarget.dataset.company,
@@ -26,31 +26,27 @@ export default class Work extends PureComponent {
         console.log(this.state);
     }
 
-    changeColor = (position) => {
-        if(this.state.selected === position) {
-            return "#ed4b64";
-        } 
-        return "#2d2a32";
-    }
+    // changeColor = (position) => {
+    //     if(this.state.selected === position) {
+    //         return "#ed4b64";
+    //     } 
+    //     return "#2d2a32";
+    // }
 
     render() {
         return (
             <section id="experience" className="work">
                 <h2>My Work Experience:</h2>
                 <ul className="experience">
-                    <li style={{color: this.changeColor(0)}} 
-                    position="0"
+                    <li 
                     data-title="Tech Producer" data-company="Helios Interactive" data-description="Part scrum master and part release manager, I worked with clients, producers, designers, and developers to keep projects on schedule and deliver working apps under strict deadlines." data-dates="July 2016 - Jan 2019" onClick={this.handleClick}>Tech Producer - Helios Interactive</li>
-                    <li style={{color: this.changeColor(1)}} 
-                    position="1" 
+                    <li  
                     data-title="Digital Director" data-company="Roger That Agency" data-description="As Digital Director at Roger That, I was responsible for sourcing and managing developers for clients’ web projects. I also wrote web copy, tracked site traffic and SEO metrics, and planned and launched PPC campaigns." data-dates="Nov 2014 - June 2016" onClick={this.handleClick}>Digital Director - Roger That Agency</li>
-                    <li style={{color: this.changeColor(2)}} 
-                    position="2" 
+                    <li  
                     data-title="Social Media Manager" data-company="Pink Martini/Heinz Records" data-description="As Social Media Manager at Pink Martini, I was responsible for managing the social media accounts for Pink Martini and Heinz Records, including planning and executing digital giveaways, writing and distributing blog content, and making HTML/CSS updates to their websites." data-dates="May 2013 - Sept 2014" onClick={this.handleClick}>Social Media Manager - Pink Martini</li>
-                    <li style={{color: this.changeColor(3)}}
-                    position="3"  data-title="Founder/Lead SEO" data-company="Foster Digital Marketing" data-description="As a freelance digital marketer and web producer I was responsible for taking clients through the lifecycle of a web development project, from key messaging to UX and design through development, QA, and deployment. I also performed SEO site audits on existing websites to help companies’ sites perform better in search results, implemented and managed social media and PPC campaigns, and wrote blog posts, articles, and website copy." data-dates="Sept 2010 - Oct 2014" onClick={this.handleClick}>Founder - Foster Digital Marketing</li>
-                    <li style={{color: this.changeColor(4)}}  
-                    position="4"
+                    <li 
+                     data-title="Founder/Lead SEO" data-company="Foster Digital Marketing" data-description="As a freelance digital marketer and web producer I was responsible for taking clients through the lifecycle of a web development project, from key messaging to UX and design through development, QA, and deployment. I also performed SEO site audits on existing websites to help companies’ sites perform better in search results, implemented and managed social media and PPC campaigns, and wrote blog posts, articles, and website copy." data-dates="Sept 2010 - Oct 2014" onClick={this.handleClick}>Founder - Foster Digital Marketing</li>
+                    <li 
                     data-title="Copywriter" data-company="Mad Fish Digital" data-description="As a copywriter at Mad Fish, I was responsible for writing and styling posts for a variety of B2C and B2B blogs." data-dates="May 2008 - May 2011"  onClick={this.handleClick} >Copywriter - Mad Fish Digital</li>
                 </ul>
                 <section className="job-blurb">
