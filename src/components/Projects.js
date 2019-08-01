78f9069d9c677a0a77f48ec81396997bfcb47583
Ben Parisot <me@benparisot.com>
2019-07-31 20:05:48 -0700

    fix typo on robot haikubot project title

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,7 +11,7 @@ export default function Projects() {
                 <h2>Recent Projects:</h2>
                 <section id="projects" className="projects">
                     <Project title="Mega Nap" image={meganap} description="A web app that lets front end developers generate API endpoints from any data set." demo="Live Demo" demoLink="https://www.meganap.com/" repo="Github Repo" repoLink="https://github.com/grandpapi" color="#ed4b64" />
-                    <Project title="Robot Haikut" image={robot} description="A Twitter bot that sends a randomly-generated haiku to those that ask for one." demo="Twitter Account" demoLink="https://twitter.com/RobotHaikubot" repo="Github Repo" repoLink="https://github.com/team-haikubot" color="#f26639" />
+                    <Project title="Robot Haikubot" image={robot} description="A Twitter bot that sends a randomly-generated haiku to those that ask for one." demo="Twitter Account" demoLink="https://twitter.com/RobotHaikubot" repo="Github Repo" repoLink="https://github.com/team-haikubot" color="#f26639" />
                     <Project title="Track 2020" image={track2020} description="An app to help people track their favorite candidates during the 2020 democratic primary." demo="In Development" repo="Github Repo" repoLink="https://github.com/track-2020" color="#f04d4d" />
                     <Project title="Pokematch" image={pokemon} description="A Pokemon matching game to teach kids numbers and pattern recognition." demo="In Development" repo="Github Repo" color="#ed4b64" />
                 </section>
