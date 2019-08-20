e4132c620f034e6f62db101a67c32ccc58a906c4
Ben Parisot <me@benparisot.com>
2019-08-20 10:43:46 -0700

    update work tech section

--- src/components/About.js
+++ src/components/About.js
@@ -24,7 +24,7 @@ export default function About() {
                         <li>Jest/Q-Unit</li>
                         <li>MongoDB/Firebase/AWS</li>
                         <li>Travis</li>
-                        <li>TDD/DDD</li>
+                        <li>TDD</li>
                     </ul>
                 </section>
                 <section className="tools">
